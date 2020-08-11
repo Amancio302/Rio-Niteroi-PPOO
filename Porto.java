b0eb2c0582f3615524bbffff2fe51aa7af6959e4
Victor Cabral <victorcabral029@gmail.com>
2020-08-11 10:17:14 -0300

    Add files via upload
    "Porto" Class done

---
+++ Porto.java
@@ -0,0 +1,12 @@
+public class Porto{
+	
+	private ArrayList<Balsa> balsasDisponiveis;
+	private ArrayList<Veiculo> filaComum;
+	private ArrayList<Veiculo> filaExpress;
+	
+	public void entrarNaFila(Veiculo v, String fila){}
+	public void sairDaFila(Veiculo v){}
+	public void embarcarBalsa(Balsa b){}
+	public void desembarcarBalsa(Balsa b){}
+}
+
